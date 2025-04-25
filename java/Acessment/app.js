c3ecc7e2529095a8abbd5aede8174965255b027e
patelheni <patelheni2003@gmail.com>
2025-04-25 14:21:01 +0530

    commit

---
+++ java/Acessment/app.js
@@ -0,0 +1,42 @@
+const input = document.querySelector("#input");
+const add = document.querySelector("#Add");
+const container = document.querySelector(".container-body");
+
+window.addEventListener("keydown", (e) => {
+    // console.log(e.key);
+    if (e.key == "Enter") {
+        addNotes();
+    }
+})
+add.addEventListener("click", () => {
+    addNotes()
+});
+function addNotes() {
+    let todo = input.value.trim();
+    if (todo) {
+        let newNode = document.createElement("li");
+
+        container.appendChild(newNode);
+
+        let delBtn = document.createElement("button");
+        delBtn.innerHTML = "&times;";
+        delBtn.className = "deleBtn";
+        newNode.className = "node";
+        newNode.textContent = todo;
+        container.appendChild(delBtn);
+
+        input.value = "";
+
+        let cancel = document.querySelectorAll(".deleBtn");
+        let nodes = document.querySelectorAll(".node");
+
+        cancel.forEach((e, i) => {
+            e.addEventListener("click", () => {
+                container.removeChild(cancel[i])
+                container.removeChild(nodes[i])
+            })
+        });
+    } else {
+        alert("enter your todays task");
+    }
+}
