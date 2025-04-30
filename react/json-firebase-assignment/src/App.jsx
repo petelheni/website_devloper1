1fcdfe26f8c6fe7eac9ee6314cbfed6cfb8e3cb3
patelheni <patelheni2003@gmail.com>
2025-04-30 11:41:05 +0530

    cha

--- react/json-firebase-assignment/src/App.jsx
+++ react/json-firebase-assignment/src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { useEffect } from "react";
+import AddUser from "./components/AddUser";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    role: "",
+    city: "",
+  });
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [updateState, setUpdateState] = useState(false);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("http://localhost:3000/users")
+        .then((res) => res.json())
+        .then((data) => setUserData(data));
+    };
+    fetchData();
+  }, [updateState]);
 
+  const handleDelete = (id) => {
+    setUserData((prev) => prev.filter((user) => user.id !== id));
+    fetch(`http://localhost:3000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.ok) {
+          alert("data deleted succesfully.");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleUpdate = (user) => {
+    setUser({
+      id: user.id,
+      name: user.name,
+      role: user.role,
+      city: user.city,
+    });
+    setIsUpdating(true);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <AddUser
+        userDetail={[user, setUser]}
+        setUpdateState={setUpdateState}
+        updatingUser={[isUpdating, setIsUpdating]}
+      />
+
+      {!userData ? (
+        <h2 className="p-4 text-center">Loading...</h2>
+      ) : (
+        <div className="shadow py-3 px-5 container mt-5">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Role</th>
+                <th>City</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userData.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.role}</td>
+                  <td>{user.city}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger me-2"
+                      onClick={() => handleDelete(user.id)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => handleUpdate(user)}
+                    >
+                      Update
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ react/json-firebase-assignment/src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC-tdR06tUXRaW4ec2e2nf3RMRpWku6xAk",
+    authDomain: "assignment-d7a98.firebaseapp.com",
+    projectId: "assignment-d7a98",
+    storageBucket: "assignment-d7a98.firebasestorage.app",
+    messagingSenderId: "984457029543",
+    appId: "1:984457029543:web:f4e58044d1f819e33b7882"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
